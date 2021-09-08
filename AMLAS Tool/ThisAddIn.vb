60adc6ff50439006753d8ecdcb9f86c9aca2713b
amlas-tool <kester.clegg@york.ac.uk>
2021-09-08 09:53:14 +0100

    Add project files.

---
+++ AMLAS Tool/ThisAddIn.vb
@@ -0,0 +1,11 @@
+﻿Public Class ThisAddIn
+
+    Private Sub ThisAddIn_Startup() Handles Me.Startup
+        MsgBox("testing", Title:="Test me!")
+    End Sub
+
+    Private Sub ThisAddIn_Shutdown() Handles Me.Shutdown
+
+    End Sub
+
+End Class
