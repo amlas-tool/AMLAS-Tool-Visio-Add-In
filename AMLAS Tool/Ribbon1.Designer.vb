e682ddb5909124705e49c28767e2f20533182cb4
amlas-tool <kester.clegg@york.ac.uk>
2021-09-13 22:20:09 +0100

    Add ribbon

---
+++ AMLAS Tool/Ribbon1.Designer.vb
@@ -0,0 +1,97 @@
+﻿Partial Class AMLAS
+    Inherits Microsoft.Office.Tools.Ribbon.RibbonBase
+
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Public Sub New(ByVal container As System.ComponentModel.IContainer)
+        MyClass.New()
+
+        'Required for Windows.Forms Class Composition Designer support
+        If (container IsNot Nothing) Then
+            container.Add(Me)
+        End If
+
+    End Sub
+
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Public Sub New()
+        MyBase.New(Globals.Factory.GetRibbonFactory())
+
+        'This call is required by the Component Designer.
+        InitializeComponent()
+
+    End Sub
+
+    'Component overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Component Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Component Designer
+    'It can be modified using the Component Designer.
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Tab1 = Me.Factory.CreateRibbonTab
+        Me.Navigation = Me.Factory.CreateRibbonGroup
+        Me.Button1 = Me.Factory.CreateRibbonButton
+        Me.Tab1.SuspendLayout()
+        Me.Navigation.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Tab1
+        '
+        Me.Tab1.ControlId.ControlIdType = Microsoft.Office.Tools.Ribbon.RibbonControlIdType.Office
+        Me.Tab1.Groups.Add(Me.Navigation)
+        Me.Tab1.Label = "TabAddIns"
+        Me.Tab1.Name = "Tab1"
+        '
+        'Navigation
+        '
+        Me.Navigation.Items.Add(Me.Button1)
+        Me.Navigation.Label = "Navigation"
+        Me.Navigation.Name = "Navigation"
+        '
+        'Button1
+        '
+        Me.Button1.ControlSize = Microsoft.Office.Core.RibbonControlSize.RibbonControlSizeLarge
+        Me.Button1.Label = "Test"
+        Me.Button1.Name = "Button1"
+        Me.Button1.ShowImage = True
+        '
+        'AMLAS
+        '
+        Me.Name = "AMLAS"
+        Me.RibbonType = "Microsoft.Visio.Drawing"
+        Me.Tabs.Add(Me.Tab1)
+        Me.Tab1.ResumeLayout(False)
+        Me.Tab1.PerformLayout()
+        Me.Navigation.ResumeLayout(False)
+        Me.Navigation.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents Tab1 As Microsoft.Office.Tools.Ribbon.RibbonTab
+    Friend WithEvents Navigation As Microsoft.Office.Tools.Ribbon.RibbonGroup
+    Friend WithEvents Button1 As Microsoft.Office.Tools.Ribbon.RibbonButton
+End Class
+
+Partial Class ThisRibbonCollection
+
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Friend ReadOnly Property Ribbon1() As AMLAS
+        Get
+            Return Me.GetRibbon(Of AMLAS)()
+        End Get
+    End Property
+End Class
--- AMLAS Tool/ThisAddIn.vb
+++ AMLAS Tool/ThisAddIn.vb
@@ -1,7 +1,8 @@
 ﻿Public Class ThisAddIn
 
     Private Sub ThisAddIn_Startup() Handles Me.Startup
-        MsgBox("testing", Title:="Test me!")
+
+
     End Sub
 
     Private Sub ThisAddIn_Shutdown() Handles Me.Shutdown
