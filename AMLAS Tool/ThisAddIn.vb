b84341d5eaa7f0f7b9335f400eddfa60684e9ad6
amlas-tool <kester.clegg@york.ac.uk>
2021-10-01 14:14:14 +0100

    fixed justification shape not updating background

--- AMLAS Tool/ThisAddIn.vb
+++ AMLAS Tool/ThisAddIn.vb
@@ -31,26 +31,35 @@ Public Class ThisAddIn
     Private Sub Application_ShapeAdded(Shape As Shape) Handles Application.ShapeAdded
         Dim vsoMaster As Visio.Master
         Dim visioDocs As Visio.Documents = Me.Application.Documents
+        Dim activePage As String
 
+        activePage = Globals.ThisAddIn.Application.ActivePage.Name
         'Get the Master property of the shape. 
         vsoMaster = Shape.Master
+
+
 
         'Check whether the shape has a master. If not, 
         'the shape was created locally. 
         If Not vsoMaster Is Nothing Then
-
-            For i = 1 To stages.Length - 1
-                If AMLAS_Tool.Globals.ThisAddIn.Application.ActivePage.Name.Contains(stages(i)) Then
-                    If Not vsoMaster.Name.Contains("Document") Then
-                        For Each shapewithin As Visio.Shape In Shape.Shapes
-                            shapewithin.CellsU("Fillforegnd").FormulaU = stage_colour(i)
-                            shapewithin.CellsU("FillBkgnd").FormulaU = "THEMEGUARD(SHADE(FillForegnd,LUMDIFF(THEMEVAL(""FillColor""),THEMEVAL(""FillColor2""))))"
-                        Next shapewithin
+                'create background colour for dropped shapes
+                For i = 1 To stages.Length - 1
+                If activePage.Contains(stages(i)) Then
+                    If Not vsoMaster.Name.Contains("Document") Then 'documents have no colour
+                        If vsoMaster.Name.Contains("Justification") Or vsoMaster.Name.Contains("Assumption") Then
+                            For Each shapewithin As Visio.Shape In Shape.Shapes
+                                shapewithin.CellsU("Fillforegnd").FormulaU = stage_colour(i)
+                                'shapewithin.CellsU("FillBkgnd").FormulaU = "THEMEGUARD(SHADE(FillForegnd,LUMDIFF(THEMEVAL(""FillColor""),THEMEVAL(""FillColor2""))))"
+                            Next shapewithin
+                        Else
+                            Shape.CellsU("Fillforegnd").FormulaU = stage_colour(i)
+                        End If
                     End If
                 End If
+
             Next i
 
-        End If
+            End If
 
     End Sub
 End Class
